563f0ada7ac5f460780992f86151492c3be60c6e
julia8873 <jmiralles8873@correo.ugr.es>
2026-09-18 07:28:38 +0200

    quitado ficheros que no sirven

--- src/bot/fix_db2.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('/data/maubot.db')
-cursor = conn.cursor()
-cursor.execute("UPDATE client SET next_batch = '', filter_id = '' WHERE id = '@llm_wiki_bot:localhost'")
-conn.commit()
-conn.close()
