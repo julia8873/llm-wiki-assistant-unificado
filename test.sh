346d342ca28a162d0246c42aa952b09fc2c915e8
Julia <jmiralles8873@correo.ugr.es>
2026-09-18 19:04:01 +0200

    Eliminar script de pruebas

--- test.sh
+++
@@ -1,6 +0,0 @@
-m_pass="maubotpass_changeme"
-docker run --rm alpine sh -c "
-  apk add --no-cache python3 py3-bcrypt >/dev/null 2>&1 || true
-  m_hash=\$(python3 -c \"import bcrypt; print(bcrypt.hashpw('${m_pass}'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'))\" 2>/dev/null)
-  echo \$m_hash
-"
